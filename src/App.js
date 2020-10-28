f68c4c5be022d2e7e78037b447b30cdc57a51783
Merlog <jakub.fides@gmail.com>
2020-10-29 00:09:43 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Header from './components/header/Header';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <div class="container" >
+        <div class="row">
+
+          <div class="col col-lg-3 ">
+            tralala
+            {/* <!-- LEVA NAVIGACE -->
+    <iframe src="navLeftMTG.html"
+      onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
+    <iframe src="navLeftProdukty.html"
+      onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
+    <!-- <iframe src="navLeftEdice.html"
+      onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe> --> */}
+          </div>
+
+          {/* <iframe src="CarouselModule.html"
+    onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe> */}
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
