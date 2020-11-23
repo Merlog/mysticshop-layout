7d78c3c29433d3b70572ebaf63363a96aa7d4961
Merlog <jakub.fides@gmail.com>
2020-11-23 23:01:35 +0100

    L3 menu edice

---
+++ src/components/L3menu.js
@@ -0,0 +1,34 @@
+import "./L2menu.css";
+
+export default function L2menu() {
+  return (
+    <nav className="nav flex-column navEdice">
+      <a className="nav-link" href="#">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-tree-fill" fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+            d="M8 0a.5.5 0 0 1 .416.223l3 4.5A.5.5 0 0 1 11 5.5h-.098l2.022 3.235a.5.5 0 0 1-.424.765h-.191l1.638 3.276a.5.5 0 0 1-.447.724h-11a.5.5 0 0 1-.447-.724L3.69 9.5H3.5a.5.5 0 0 1-.424-.765L5.098 5.5H5a.5.5 0 0 1-.416-.777l3-4.5A.5.5 0 0 1 8 0z" />
+          <path d="M7 13h2v3H7v-3z" />
+        </svg>
+    Promo karty</a>
+      <a className="nav-link" href="#">Core Set 2021</a>
+      <a className="nav-link" href="#">Ikoria: Lair of Behemoths</a>
+      <a className="nav-link" href="#">Theros Beyond Death</a>
+      <a className="nav-link" href="#">Theros Beyond Death (Alternate)</a>
+      <a className="nav-link" href="#">Throne of Eldraine</a>
+      <a className="nav-link" href="#">Throne of Eldraine (Alternate)</a>
+      <a className="nav-link" href="#">Core Set 2020</a>
+      <a className="nav-link" href="#">Modern Horizons</a>
+      <a className="nav-link" href="#">War of the Spark</a>
+      <a className="nav-link" href="#">Ravnica Allegiance</a>
+      <a className="nav-link" href="#">Ultimate Masters</a>
+      <a className="nav-link" href="#">Guilds of Ravnica</a>
+      <a className="nav-link" href="#">Core Set 2019</a>
+      <a className="nav-link" href="#">Battlebond</a>
+      <a className="nav-link" href="#">Dominaria</a>
+      <a className="nav-link" href="#">Masters 25</a>
+      <a className="nav-link" href="#">Rivals of Ixalan</a>
+      <a className="nav-link" href="#">Unstable</a>
+    </nav>
+  );
+}
--- src/pages/HomepagePage.js
+++ src/pages/HomepagePage.js
@@ -1,5 +1,6 @@
 import L1menu from "./../components/L1menu";
 import L2menu from "./../components/L2menu";
+import L3menu from "./../components/L3menu";
 import Carousel from "./../components/Carousel";
 
 export default function HomepagePage() {
@@ -9,6 +10,7 @@ export default function HomepagePage() {
         <div class="col col-lg-3 ">
           <L1menu />
           <L2menu />
+          <L3menu />
         </div>
         <Carousel />
       </div>
