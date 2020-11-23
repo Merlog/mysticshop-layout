c53866cd1ff9e1c182a934169159fc5151a81a2b
Kateřina Faustenová <faustenova@fg.cz>
2020-11-23 22:20:35 +0100

    order list styling

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -1,48 +1,51 @@
 export default function OrderList() {
+  // chybí onclick na každý řádek (na danou položku), který otevře (route) na order detail s danou objednávkou
   return (
     <div class="container" style={{ marginTop: "360px" }}>
       <div class="row">
-        <h5 class="text-center">Seznam objednávek</h5>
-        <div class="col-12">
+       <div class="col-12 mb-5">
+          <h4 class="text-center">Přehled objednávek</h4>
+        </div>
+        <div class="col-8 offset-2 mb-5">
           <table class="table table-hover table-sm">
-            <thead class="thead-light">
+            <thead class="thead-dark">
               <tr>
-                <th scope="col">Číslo obj.</th>
-                <th scope="col">Datum</th>
-                <th scope="col">Stav</th>
-                <th scope="col">Celkem</th>
+                <th scope="col">Číslo objednávky</th>
+                <th scope="col" class="text-center">Datum</th>
+                <th scope="col" class="text-center">Stav</th>
+                <th scope="col" class="text-right">Celkem</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <th scope="row">1111</th>
-                <td>1. 1. 2020</td>
-                <td>zaplacená</td>
-                <td>1 248 Kč</td>
+                <td class="text-center">1. 1. 2020</td>
+                <td class="text-center">zaplacená</td>
+                <td class="text-right">1 248 Kč</td>
               </tr>
               <tr>
                 <th scope="row">2222</th>
-                <td>1. 5. 2020</td>
-                <td>zaplacená</td>
-                <td>348 Kč</td>
+                <td class="text-center">1. 5. 2020</td>
+                <td class="text-center">zaplacená</td>
+                <td class="text-right">348 Kč</td>
               </tr>
               <tr>
                 <th scope="row">3333</th>
-                <td>10. 9. 2020</td>
-                <td>zaplacená</td>
-                <td>1 548 Kč</td>
+                <td class="text-center">10. 9. 2020</td>
+                <td class="text-center">zaplacená</td>
+                <td class="text-right">1 548 Kč</td>
               </tr>
               <tr>
                 <th scope="row">3333</th>
-                <td>10. 9. 2020</td>
-                <td>zaplacená</td>
-                <td>1 548 Kč</td>
+                <td class="text-center">10. 9. 2020</td>
+                <td class="text-center">zaplacená</td>
+                <td class="text-right">1 548 Kč</td>
               </tr>
               <tr>
                 <th scope="row">3333</th>
-                <td>10. 9. 2020</td>
-                <td>zaplacená</td>
-                <td>1 548 Kč</td>
+                <td class="text-center">10. 9. 2020</td>
+                <td class="text-center">čeká na platbu</td>
+                <td class="text-right">1 548 Kč</td>
               </tr>
             </tbody>
           </table>
