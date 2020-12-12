b5b3d9d97d2a84e67335460fb118d966d6424360
Kateřina Faustenová <faustenova@fg.cz>
2020-12-12 14:56:53 +0100

    header logo, search, bonus & cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import "./Header.css";
 import logo from "./../assets/logo1.png";
+import cart from "./../assets/svg/cart-material.svg";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import Search from "./../assets/svg/search-material.svg";
 
 export default function Header() {
   const [loggedIn, setLoggedIn] = useState(true);
@@ -58,44 +60,38 @@ export default function Header() {
               </ul>
             </div>
           </div>
-          <div className="row navMidle">
-            <div className="col-2">
+          <div className="row navMidle mb-3">
+            <div className="col-3">
               <a className="navbar-brand mainLogo" href="/#">
                 <img src={logo} alt="logo" />
               </a>
             </div>
-            <div className="col-5 my-3">
-              <form className="form-inline my-2 my-lg-0">
+            <div className="col-6">
+              <form className="form-inline my-2 searchWrapper">
                 <input
-                  className="form-control mr-sm-2 customW400"
+                  className="form-control w-100"
                   type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-dark my-2 my-sm-0 searchButton" type="submit">
-                  Hledat
-                  </button>
+                  placeholder="Vyhledat"
+                ></input>
+                <button className="btn searchButton" type="submit">
+                  <img src={Search} alt="search"></img>
+                </button>
               </form>
             </div>
-            <div className="col-2 bonus">
-              <a className="" href="/#">
-                Bonus: 20 bodů
-              </a>
-            </div>
-            <div className="col-3 cart">
-              <a className="Kosik" href="/#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 48 48"
-                  className="cart-ico"
-                >
-                  <path d="M14 36c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4zm-12-32v4h4l7.19 15.17-2.7 4.9c-.31.58-.49 1.23-.49 1.93 0 2.21 1.79 4 4 4h24v-4h-23.15c-.28 0-.5-.22-.5-.5 0-.09.02-.17.06-.24l1.79-3.26h14.9c1.5 0 2.81-.83 3.5-2.06l7.15-12.98c.16-.28.25-.61.25-.96 0-1.11-.9-2-2-2h-29.57l-1.9-4h-6.53zm32 32c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4z" />
-                  <path d="M0 0h48v48h-48z" fill="none" />
-                </svg>
-                Košík: 159 kč
-              </a>
+            <div className="col-3 d-flex flex-row justify-content-end">
+              <div className="bonus">
+                <NavLink to="/MyAccount" exact>
+                  <span>Bonus: </span>
+                  <span className="cartAmount">20 b.</span>
+                </NavLink>
+              </div>
+              <div className="cart">
+                <NavLink to="/Cart1" exact>
+                  <img src={cart} alt="logo" />
+                  <span className="badge cartItems">1 ks</span>
+                  <span className="cartAmount">159 Kč</span>
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
