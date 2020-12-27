35f0fe0d7d4c51df086d6026587a054b173911e7
Kateřina Faustenová <faustenova@fg.cz>
2020-12-27 11:40:04 +0100

    product sections polish

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -1,12 +1,11 @@
-import "./Recommended.css";
+import "./Products.css";
 import BoosterPack from "./../assets/images/product-img/booster_pack.jpg";
 import Booster from "./../assets/images/product-img/booster.jpg";
 import Duel from "./../assets/images/product-img/duel.jpg";
-import Obal from "./../assets/images/product-img/obal.jpg";
 
 export default function Recommended() {
   return (
-    <div className="row recommended">
+    <div className="row products">
       <div className="col-12">
         <div className="row title">
           <div className="col-11">
@@ -41,96 +40,230 @@ export default function Recommended() {
 
         <div className="row">
           <div className="col-2">
-            <div className="card">
-              <img src={Booster} className="card-img-top" alt="recommended" />
-              <div className="card-body">
-                <h5 className="card-title">Core Set 2021 booster</h5>
-                <h5 className="card-price">99 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={Booster}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Core Set 2021 booster
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">99 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
 
           <div className="col-2">
-            <div className="card">
-              <img
-                src={BoosterPack}
-                className="card-img-top"
-                alt="recommended"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Core Set 2021 booster box</h5>
-                <h5 className="card-price">2 799 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={BoosterPack}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Core Set 2021 booster box
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">2 799 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
 
           <div className="col-2">
-            <div className="card">
-              <img src={Duel} className="card-img-top" alt="recommended" />
-              <div className="card-body">
-                <h5 className="card-title">Duel Decks: Elves Vs. Inventors</h5>
-                <h5 className="card-price">449 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={Duel}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Duel Decks: Elves Vs. Inventors
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">449 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
 
           <div className="col-2">
-            <div className="card">
-              <img src={Obal} className="card-img-top" alt="recommended" />
-              <div className="card-body">
-                <h5 className="card-title">Duel Decks: Elves Vs. Inventors</h5>
-                <h5 className="card-price">449 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={Duel}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Duel Decks: Elves Vs. Inventors
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">449 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
 
           <div className="col-2">
-            <div className="card">
-              <img src={Obal} className="card-img-top" alt="recommended" />
-              <div className="card-body">
-                <h5 className="card-title">Duel Decks: Elves Vs. Inventors</h5>
-                <h5 className="card-price">449 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={Duel}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Duel Decks: Elves Vs. Inventors
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">449 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
 
           <div className="col-2">
-            <div className="card">
-              <img src={Obal} className="card-img-top" alt="recommended" />
-              <div className="card-body">
-                <h5 className="card-title">Duel Decks: Elves Vs. Inventors</h5>
-                <h5 className="card-price">449 Kč</h5>
-                <div className="card-btn cardButton">
-                  <a href="#" className="btn btn-sm">
-                    Detail
-                  </a>
+            <div className="card cardModif">
+              <a href="#">
+                <img
+                  src={Duel}
+                  className="card-img-top cardImg"
+                  alt="recommended"
+                />
+                <div className="card-body d-flex flex-column cardBody">
+                  <h5 className="card-title cardTitle">
+                    Duel Decks: Elves Vs. Inventors
+                  </h5>
+                  <div className="d-flex flex-column align-items-end">
+                    <h5 className="card-price">449 Kč</h5>
+                    <div className="d-flex">
+                      <input
+                        type="number"
+                        value="1"
+                        placeholder="ks"
+                        className="cardInput"
+                      />
+                      <button
+                        type="button"
+                        className="btn card-btn cardButton"
+                        onClick={(event) => {
+                          alert("koupeno");
+                          event.preventDefault();
+                        }}
+                      >
+                        Koupit
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
         </div>
