7ca36f25ce6d5fb250b069cf51d8f7c591850541
Kateřina Faustenová <faustenova@fg.cz>
2020-12-30 10:42:35 +0100

    my account left menu

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import "./MyAccount.css";
+
 export default function MyAccount() {
-  return <div>Můj účet</div>;
+  const [orders, setOrders] = useState(false);
+  return (
+    <div className="row">
+      <div className="col-2 p-0 accountMenu">
+        <button
+          onClick={() => {
+            setOrders(false);
+          }}
+        >
+          <h5 className={`accountTitle ${orders ? "" : "accountTitleActive"}`}>
+            Můj účet
+          </h5>
+        </button>
+        <button
+          onClick={() => {
+            setOrders(true);
+          }}
+        >
+          <h5 className={`accountTitle ${orders ? "accountTitleActive" : ""}`}>
+            Mé objednávky
+          </h5>
+        </button>
+      </div>
+      {orders ? (
+        <div className="col-10">obsah k objednávkám</div>
+      ) : (
+        <div className="col-10">obsah k účtu</div>
+      )}
+    </div>
+  );
 }
