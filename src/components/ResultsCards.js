99eb0596e20b657b0c2ff8f67ece88c83bfd99d9
Kateřina Faustenová <faustenova@fg.cz>
2021-01-01 14:09:54 +0100

    result card list

--- src/components/ResultsCards.js
+++ src/components/ResultsCards.js
@@ -1,6 +1,10 @@
 import Booster from "./../assets/images/product-img/booster.jpg";
+import Mana from "./../assets/svg/mana-placeholder-material.svg";
+import Zendikar from "./../assets/images/zendikar.jpg";
 import Pagination from "./Pagination";
 import { useState } from "react";
+import cart from "./../assets/svg/cart-material.svg";
+import cart2 from "./../assets/svg/cart-material2.svg";
 import "./Results.css";
 
 export default function ResultsCard(props) {
@@ -21,7 +25,17 @@ export default function ResultsCard(props) {
                 />
               </div>
               <div className="col-4">
-                <p>Mana</p>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm3.23 15.39L12 15.45l-3.22 1.94c-.38.23-.85-.11-.75-.54l.85-3.66-2.83-2.45c-.33-.29-.15-.84.29-.88l3.74-.32 1.46-3.45c.17-.41.75-.41.92 0l1.46 3.44 3.74.32c.44.04.62.59.28.88l-2.83 2.45.85 3.67c.1.43-.36.77-.74.54z" />
+                  </svg>
+                </div>
                 <p>
                   <span>Set/</span>
                   <span>R</span>
@@ -79,30 +93,34 @@ export default function ResultsCard(props) {
                   alt="recommended"
                 />
               </div>
-              <div className="col-4 d-flex flex-column">
+              <div className="col-4 d-flex flex-column justify-content-around">
                 <h5 className="card-title cardTitleList">
-                  Název - Core Set 2021 booster
+                  Abbey Matron
+                  <img
+                    src={Zendikar}
+                    className="mx-2"
+                    alt="mana"
+                    width="20px"
+                  />
                 </h5>
-                <h6>Typ</h6>
+                <h6>Creature — Human Cleric (1/3)</h6>
               </div>
-              <div className="col-3 d-flex flex-column">
-                <p>Mana</p>
-                <p>
-                  <span>Set/</span>
-                  <span>R</span>
+              <div className="col-3 d-flex flex-column justify-content-around">
+                <div>
+                  <img src={Mana} className="" alt="mana" width="20px" />
+                  <img src={Mana} className="" alt="mana" width="20px" />
+                  <img src={Mana} className="" alt="mana" width="20px" />
+                </div>
+                <p>NM, foil</p>
+              </div>
+              <div className="col-2 d-flex flex-column justify-content-end text-center">
+                <h5 className="card-price">1199 Kč</h5>
+              </div>
+              <div className="col-2 d-flex flex-column justify-content-around">
+                <p className="cardTextList cardBuyList">
+                  {props.sellCards ? `15ks poptáváno` : `15ks skladem`}
                 </p>
-              </div>
-              <div className="col-2 d-flex flex-column">
-                <p>Poznámka</p>
-                <p>
-                  {props.sellCards
-                    ? `Poptávaný počet ks`
-                    : `Počet ks k dispozici`}
-                </p>
-              </div>
-              <div className="col-2">
-                <div className="d-flex flex-column align-items-end">
-                  <h5 className="card-price">99 Kč</h5>
+                <div className="d-flex flex-column align-items-end cardBuyList">
                   <div className="d-flex">
                     <input
                       type="number"
@@ -118,7 +136,16 @@ export default function ResultsCard(props) {
                         event.preventDefault();
                       }}
                     >
-                      {props.sellCards ? `Prodat` : `Koupit`}
+                      {props.sellCards ? (
+                        `Prodat`
+                      ) : (
+                        <img
+                          src={cart}
+                          alt="logo"
+                          className="dark"
+                          width="20px"
+                        />
+                      )}
                     </button>
                   </div>
                 </div>
