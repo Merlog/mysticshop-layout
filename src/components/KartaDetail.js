ad2a3aa8d37c9c526bd7f152dfc7c952f6dab686
Kateřina Faustenová <faustenova@fg.cz>
2021-01-02 21:09:11 +0100

    card detail language toggle

--- src/components/KartaDetail.js
+++ src/components/KartaDetail.js
@@ -1,7 +1,8 @@
 import Karta from "./../assets/images/karta.jpg";
 import Zendikar from "./../assets/images/zendikar.jpg";
 import { useState } from "react";
-import './KartaDetail.css'
+import cart from "./../assets/svg/cart-material.svg";
+import "./KartaDetail.css";
 
 export default function KartaDetail() {
   const [language, setLanguage] = useState(false);
@@ -11,74 +12,100 @@ export default function KartaDetail() {
         <div className="row productDetail">
           <div className="col-4 gallery">
             <div className="row mainImg">
-              <img
-                src={Karta}
-                className="imageKarta"
-                alt="recommended"
-              />
+              <img src={Karta} className="imageKarta" alt="recommended" />
             </div>
           </div>
-          <div className="col-8 content">
+          <div className="col-6 content">
             <h2>
               Stoneforge Mystic
-              <img
-                src={Zendikar}
-                className="symbol"
-                alt="recommended"
-              /></h2>
+              <img src={Zendikar} className="symbol" alt="recommended" />
+            </h2>
             <p className="productCode">Kód Karty: 230589</p>
-            <a
-              href
-              className={`linkcurrency${language ? " active" : ""}`}
-              onClick={() => {
-                setLanguage(!language);
-              }}
-            >
-              cz
-            </a>
+            {language ? (
+              <a
+                href
+                className="language"
+                onClick={() => {
+                  setLanguage(!language);
+                }}
+              >
+                CZ
+              </a>
+            ) : (
+              <a
+                href
+                className="language"
+                onClick={() => {
+                  setLanguage(!language);
+                }}
+              >
+                EN
+              </a>
+            )}
             {!language && (
               <div className="text cz">
+                <p>Flying, double strike</p>
                 <p>
-
-                  Flying, double strike
-              <br />    <br />
-
-
-              Kdykoli bytost pod tvou kontrolou udělí bojové zranění hráči, ty a onen hráč si přidáte odpovídající množství životů.
-
-              <br />    <br />
-
-              Na začátku tvé fáze konce kola, pokud máš aspoň o 15 životů více než na počátku hry, každý hráč, na kterého Angel of Destiny toto kolo zaútočila, prohrává hru.
-              <br />    <br />
+                  Kdykoli bytost pod tvou kontrolou udělí bojové zranění hráči,
+                  ty a onen hráč si přidáte odpovídající množství životů.
                 </p>
-
-                <p className="italic">
-                  "Archoni vykonávají spravedlnost na základě prastarých, ale nedokonalých pravidel."
-                  —Nissa Revane
-              </p>
+                <p>
+                  Na začátku tvé fáze konce kola, pokud máš aspoň o 15 životů
+                  více než na počátku hry, každý hráč, na kterého Angel of
+                  Destiny toto kolo zaútočila, prohrává hru.
+                </p>
+                <p className="text italic">
+                  "Archoni vykonávají spravedlnost na základě prastarých, ale
+                  nedokonalých pravidel." — Nissa Revane
+                </p>
+                {/* <p>Flying, double strike</p>
+                <p>
+                  Kdykoli bytost pod tvou kontrolou udělí bojové zranění hráči,
+                  ty a onen hráč si přidáte odpovídající množství životů.
+                </p>
+                <p>
+                  Na začátku tvé fáze konce kola, pokud máš aspoň o 15 životů
+                  více než na počátku hry, každý hráč, na kterého Angel of
+                  Destiny toto kolo zaútočila, prohrává hru.
+                </p>
+                <p className="text italic">
+                  "Archoni vykonávají spravedlnost na základě prastarých, ale
+                  nedokonalých pravidel." — Nissa Revane
+                </p> */}
               </div>
             )}
             {language && (
               <div className="text en">
-
-                Flying, double strike
-                <br />    <br />
-Whenever a creature you control deals combat damage to a player, you and that player each gain that much life.
-                <br />    <br />
-At the beginning of your end step, if you have at least 15 life more than your starting life total, each player Angel of Destiny attacked this turn loses the game.
+                <p>Flying, double strike</p>
+                <p>
+                  Whenever a creature you control deals combat damage to a
+                  player, you and that player each gain that much life.
+                </p>
+                <p>
+                  At the beginning of your end step, if you have at least 15
+                  life more than your starting life total, each player Angel of
+                  Destiny attacked this turn loses the game.
+                </p>
               </div>
             )}
-            <div className="cardPrice">199 Kč</div>
-            <div className="d-flex skladem">
-              15ks skladem
           </div>
+          <div className="col-2 buy">
+            <div className="cardPrice">199 Kč</div>
+            <div className="d-flex skladem">15ks skladem</div>
             <div className="d-flex koupit">
-              <input
-                type="number"
-                value="1"
-                placeholder="ks"
-                className="cardInput"
-              />
+              <div className="quantity position-relative">
+                <input
+                  type="number"
+                  min="1"
+                  step="1"
+                  value="1"
+                  className="cardInput"
+                />
+                <div className="quantity-nav">
+                  <div className="quantity-button quantity-up">+</div>
+                  <div className="quantity-button quantity-down">-</div>
+                </div>
+              </div>
               <button
                 type="button"
                 className="btn card-btn cardButton"
@@ -87,7 +114,7 @@ At the beginning of your end step, if you have at least 15 life more than your s
                   event.preventDefault();
                 }}
               >
-                Koupit
+                <img src={cart} alt="koupit" className="dark" width="20px" />
               </button>
             </div>
           </div>
