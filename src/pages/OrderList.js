23021070a43b9e765ce5d0e10a3b35a3622276a9
Kateřina Faustenová <faustenova@fg.cz>
2021-02-21 16:36:08 +0100

    order list styles

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -3,72 +3,62 @@ import "./OrderList.css";
 
 export default function OrderList() {
   return (
-    <div className="row">
-      <div className="col mb-5">
+    <div className="row order-list">
+      <div className="col-12">
         <table className="table table-hover table-sm">
-          <thead className="thead-dark">
+          <thead className="order-list-header">
             <tr>
-              <th scope="col">Číslo objednávky</th>
-              <th scope="col" className="text-center">
-                Datum
+              <th scope="col">
+                <h4 className="mb-4">Číslo objednávky</h4>
               </th>
               <th scope="col" className="text-center">
-                Stav
+                <h4 className="mb-4">Datum</h4>
+              </th>
+              <th scope="col" className="text-center">
+                <h4 className="mb-4">Stav</h4>
               </th>
               <th scope="col" className="text-right">
-                Celkem
+                <h4 className="mb-4">Celkem</h4>
               </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="order-list-body">
             <tr>
-              <th scope="row">
-                <NavLink to="/mysticshop/OrderDetail">
-                  1111 
-
-              </NavLink>
-              </th>
+              <td>
+                <NavLink to="/mysticshop/OrderDetail">1111111</NavLink>
+              </td>
               <td className="text-center">1. 1. 2020</td>
               <td className="text-center">zaplacená</td>
               <td className="text-right">1 248 Kč</td>
             </tr>
             <tr>
-              <th scope="row">
-                <NavLink to="/mysticshop/OrderDetail">
-                  2222
-              </NavLink>
-              </th>
+              <td>
+                <NavLink to="/mysticshop/OrderDetail">2222222</NavLink>
+              </td>
               <td className="text-center">1. 5. 2020</td>
               <td className="text-center">zaplacená</td>
               <td className="text-right">348 Kč</td>
             </tr>
             <tr>
-              <th scope="row">
-                <NavLink to="/mysticshop/OrderDetail">
-                  3333
-              </NavLink>
-              </th>
+              <td>
+                <NavLink to="/mysticshop/OrderDetail">3333333</NavLink>
+              </td>
               <td className="text-center">10. 9. 2020</td>
               <td className="text-center">zaplacená</td>
               <td className="text-right">1 548 Kč</td>
             </tr>
             <tr>
-              <th scope="row">
-                <NavLink to="/mysticshop/OrderDetail">
-                  3333
-              </NavLink>
-              </th>
+              <td>
+                <NavLink to="/mysticshop/OrderDetail">3333333</NavLink>
+              </td>
               <td className="text-center">10. 9. 2020</td>
               <td className="text-center">zaplacená</td>
               <td className="text-right">1 548 Kč</td>
             </tr>
             <tr>
-              <th scope="row">
-                <NavLink to="/mysticshop/OrderDetail">
-                  3333
-              </NavLink>
-              </th>
-
+              <td>
+                <NavLink to="/mysticshop/OrderDetail">3333333</NavLink>
+              </td>
               <td className="text-center">10. 9. 2020</td>
               <td className="text-center">čeká na platbu</td>
               <td className="text-right">1 548 Kč</td>
